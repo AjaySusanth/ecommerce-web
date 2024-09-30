9b35736b60ee4b985c513c1b73ac8099f43d632f
Ajay Susanth <ajaysusanth10@gmail.com>
2024-09-30 09:46:23 +0530

    refreshToken-route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -56,14 +56,39 @@ export const signup = async (req,res)=>{
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            message:"Unexpected error occeured",
+            message:"Unexpected error occured while sign up",
             error:error.message
         })
     }
 }
 
 export const login = async (req,res)=>{
-    res.send("login route")
+    try {
+        const {email,password} = req.body
+        const user = await User.findOne({email})
+        if(!user) return res.status(404).json({
+            message:"User not found,Please Sign in"
+        })
+
+        const isPasswordCorrect = await user.comparePassword(password)
+        if(!isPasswordCorrect) return res.status(401).json({
+            message:"Incorrect password"
+        });
+
+        const {accessToken,refreshToken} = generateToken(user._id)
+        await storeRefreshToken(user._id,refreshToken)
+        setCookies(res,refreshToken,accessToken)
+
+        res.status(201).json({
+            message:"Login successful",
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message:"Unexpected error occured while logging in",
+            error:error.message
+        })
+    }
 }
 
 export const logout = async (req,res)=>{
@@ -81,6 +106,39 @@ export const logout = async (req,res)=>{
         console.log(error.message)
         res.status(500).json({
             message:"Unexpected error occured",
+            error:error.message
+        })
+    }
+}
+
+export const refreshToken = async(req,res)=> {
+    
+    try {
+        const refreshToken = req.cookies.refreshToken
+        if(!refreshToken) return res.json({message:"No refresh token"})
+        const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET)
+        const storedRefreshToken = await redis.get(`refresh_token:${decoded.userId}`)
+
+        if(storedRefreshToken!==refreshToken) res.status(401).json({
+            message:"Invalid refresh token"
+        })
+
+        const accessToken = jwt.sign({userId:decoded.userId},process.env.ACCESS_TOKEN_SECRET, {expiresIn:'15m'})
+
+        res.cookie("accessToken",accessToken,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            sameSite:'strict',
+            maxAge:15*60*1000
+        })
+        res.status(200).json({
+            accessToken,
+            message:"access token refreshed"
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message:"Unexpected error occured while refreshing token",
             error:error.message
         })
     }
