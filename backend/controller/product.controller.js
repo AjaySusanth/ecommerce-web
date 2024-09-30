bfb30d09a8afaa36bcf53486830fc8b34878db47
Ajay Susanth <ajaysusanth10@gmail.com>
2024-09-30 13:37:37 +0530

    create-product-route

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,3 +1,5 @@
+import cloudinary from "../lib/cloudinary.js"
+import { redis } from "../lib/redis.js"
 import Product from "../models/product.model.js"
 
 export const getAllProducts = async(req,res) => {
@@ -8,6 +10,56 @@ export const getAllProducts = async(req,res) => {
         console.log(error.message)
         res.status(500).json({
             message:"Unexpected error occured while getting all products",
+            error:error.message  
+        })
+    }
+}
+
+export const getFeaturedProducts = async(req,res) => {
+    try {
+        let featuredProducts = await redis.get('featured_products')
+        if(featuredProducts)
+            return res.json(JSON.parse(featuredProducts))
+        
+        featuredProducts = await Product.find({isFeatured:true}).lean()
+        //lean() returns ajs object instead of mongodb object which is good for performance
+
+        if(!featuredProducts)
+            return res.status(404).json({
+                message:"No featured products found"
+            })
+        
+        await redis.set("featured_products",JSON.stringify(featuredProducts))
+        res.json(featuredProducts)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message:"Unexpected error occured while getting featured products",
+            error:error.message  
+        })
+    }
+}
+
+export const createProduct = async(req,res) => {
+    try {
+        const {name,description,price,image,category} = req.body
+        let cloudinaryResponse = null
+        if(image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image,{folder:'products'})
+        }
+
+        const product = await Product.create({
+            name,description,price,
+            image:cloudinaryResponse?.secure_url || '',
+            category
+        })
+
+        res.status(201).json(product)
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message:"Unexpected error occured while creating products",
             error:error.message  
         })
     }
