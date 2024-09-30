1fa29e98a39fbd78c20c7eae86daf424a9ec2307
Ajay Susanth <ajaysusanth10@gmail.com>
2024-09-30 19:37:27 +0530

    delete-product-route

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -64,3 +64,32 @@ export const createProduct = async(req,res) => {
         })
     }
 }
+
+export const deleteProduct = async(req,res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if(!product) return res.status(404).json({
+            message:"Product not found"
+        })
+
+        if(product.image) {
+            const publicId = product.image.split('/').pop().split(".")[0]
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`)
+                console.log("deleted img from cloudinary")
+            } catch (error) {
+                console.log("Error deleting img from cloudinary",error.message)
+            }
+
+        }
+
+        await Product.findByIdAndDelete(req.params.id)
+        res.json({message:"Product deleted successfully"})
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message:"Unexpected error occured while deleting product",
+            error:error.message  
+        })
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createProduct, getAllProducts, getFeaturedProducts } from "../controller/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controller/product.controller.js";
 import { adminRoute, protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express.Router()
 
 router.get('/',protectRoute,adminRoute,getAllProducts)
 router.post('/',protectRoute,adminRoute,createProduct)
+router.delete('/:id',protectRoute,adminRoute,deleteProduct)
 router.get('/featured',getFeaturedProducts)
-
 export default router
